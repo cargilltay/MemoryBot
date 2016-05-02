0e2de67093fc8521b1a88e23c30671c04ef79f1b
Taylor Cargill <tcargill@bizstream.com>
2016-05-02 08:49:53 -0400

    Restructure

---
+++ MemoryBot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MargieBot.Models;
+using MargieBot.Responders;
+using MargieBot;
+using System.Threading;
+
+namespace MemoryBot
+{
+    class Program
+    {
+        //private static Timer brainTimer;
+
+        static void Main(string[] args)
+        {
+            InitBot();
+            while (true) { };
+        }
+        
+        public static async void InitBot()
+        {
+            Bot memory = new Bot();
+            InitResponses(memory);
+
+            memory.ConnectionStatusChanged += (bool isConnected) =>
+            {
+                if (isConnected)
+                    memory.Say(new BotMessage() { Text = "Hi Taylor!" });
+            };
+
+            await memory.Connect("********************************");
+        }
+
+        public static void InitResponses(Bot bot)
+        {
+            bot.Responders.Add(new Responses.MemoryMaker());
+            bot.Responders.Add(new Responses.MemoryResponder());
+        }
+    }
+}
---
+++ MemoryBot/Responses/MemoryMaker.cs
@@ -0,0 +1,58 @@
+﻿using MargieBot.Models;
+using MargieBot.Responders;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Priority_Queue;
+using System.Threading;
+
+namespace MemoryBot.Responses
+{
+    class MemoryMaker : IResponder
+    {
+        private string[] commands;
+
+        public bool CanRespond(ResponseContext context)
+        {
+            //logs messages from slack
+            //Console.WriteLine(context.Message.Text);
+            
+            bool canTalk = (context.Message.MentionsBot || context.Message.ChatHub.Type == SlackChatHubType.DM);
+            if (canTalk)
+            {
+                    return true;
+            }
+            return false;
+        }
+
+        public BotMessage GetResponse(ResponseContext context)
+        {
+            LoadCommands();
+            var msg = Utils.StringBuilder(context.Message.Text);
+            var user = context.Message.User.FormattedUserID;
+
+            if (context.Message.MentionsBot)
+            {
+                foreach (string command in commands)
+                {
+                    if (context.Message.Text.Contains(command))
+                    {
+                        Memory.WriteMemory(msg, user, DateTime.Now);
+                        return new BotMessage { Text = "I'll remind you" };
+                    }
+                }
+            }
+            return new BotMessage { Text =  string.Format("Hello {0}",user)};
+        }
+
+        private void LoadCommands()
+        {
+            using (StreamReader file = File.OpenText(@"../../commands.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                commands = (string[])serializer.Deserialize(file, typeof(string[]));
+            }
+        }
+    }
+}
